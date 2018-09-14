c57e0c660c2bb65176badb34a3407960b686f30d
Marc-Antoine Beaudoin <marc1119@live.ca>
2018-09-14 00:01:45 -0400

    Fix for merge.

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -2,14 +2,18 @@ package view;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import model.Direction;
 import model.IGameModel;
+import model.Level;
 
 public class GameView implements IGameView {
+	private final String LEVEL_SPRITES = "level_sprite";
+	private final int LEVEL_TILE_SIZE = 8;
 	private final IGameModel model;
 
 	public GameView(IGameModel model) {
@@ -25,17 +29,17 @@ public class GameView implements IGameView {
 		jFrame.setLocationRelativeTo(null);
 		jFrame.setVisible(true);
 	}
-
+/*
 	public void displayLevel() {
 		final Level level = this.model.getCurrentLevel();
 		final int width = level.getWidth();
 		final int height = level.getHeight();
-		final Sprite sprite = new Sprite(SPRITE_SOURCE);
+		final Sprite sprite = new Sprite(LEVEL_SPRITES, LEVEL_TILE_SIZE);
 		List<List<Integer>> map = level.getMap();
 
 		for (List<Integer> row : map) {
-			for (Integer column : row) {
-				// sprite.getSprite(xGrid, yGrid)
+			for (Integer spriteId : row) {
+				sprite.getSprite(spriteId, 1);
 			}
 		}
 
@@ -47,7 +51,7 @@ public class GameView implements IGameView {
 		// g2d.drawImage(source, x, y, x + width, y + height, frameX, frameY, frameX +
 		// width, frameY + height, this);
 	}
-
+*/
 	public void update() {
 
 	}
