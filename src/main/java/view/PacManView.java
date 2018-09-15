d27560035cf395676d7f4f966e209b5dd4128602
Benjamin Roy <benji015@hotmail.com>
2018-09-14 23:18:12 -0400

    Edited pacman drawing

--- src/main/java/view/PacManView.java
+++ src/main/java/view/PacManView.java
@@ -1,8 +1,7 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import model.Direction;
 import model.IGameModel;
@@ -12,10 +11,8 @@ import model.PacMan;
 public class PacManView {
 
   private IGameModel model;
-  private ImageIcon rightPacman = new ImageIcon("src\\main\\res\\pacman_right.png");
-  private ImageIcon leftPacman = new ImageIcon("src\\main\\res\\pacman_left.png");
-  private ImageIcon upPacman = new ImageIcon("src\\main\\res\\pacman_up.png");
-  private ImageIcon downPacman = new ImageIcon("src\\main\\res\\pacman_down.png");
+
+  public static final int PACMAN_RADIUS = 50;
 
   public PacManView(IGameModel model) {
     this.model = model;
@@ -29,71 +26,51 @@ public class PacManView {
       return;
     }
 
+    int xLevelPosition = pacman.getPosition().getX();
+    int yLevelPosition = pacman.getPosition().getY();
+    int levelWidth = level.getWidth();
+    int levelHeight = level.getHeight();
+    int windowWidth = window.getWidth() - PACMAN_RADIUS;
+    int windowHeight = window.getHeight() - PACMAN_RADIUS;
+    int x = (int) xLevelPosition * windowWidth / levelWidth;
+    int y = (int) yLevelPosition * windowHeight / levelHeight;
+
     if (pacman.getDirection() == Direction.RIGHT) {
-      drawRightPacman(graphic, window, pacman, level);
+      drawPacmanRight(graphic, x, y);
     } else if (pacman.getDirection() == Direction.LEFT) {
-      drawLeftPacman(graphic, window, pacman, level);
+      drawPacmanLeft(graphic, x, y);
     } else if (pacman.getDirection() == Direction.UP) {
-      drawUpPacman(graphic, window, pacman, level);
+      drawPacmanUp(graphic, x, y);
     } else if (pacman.getDirection() == Direction.DOWN) {
-      drawDownPacman(graphic, window, pacman, level);
+      drawPacmanDown(graphic, x, y);
     }
   }
 
-  private void drawRightPacman(Graphics graphic, JFrame window, PacMan pacman, Level level) {
-    Image image = rightPacman.getImage();
-    int xLevelPosition = pacman.getPosition().getX();
-    int yLevelPosition = pacman.getPosition().getY();
-    int levelWidth = level.getWidth();
-    int levelHeight = level.getHeight();
-    int windowWidth = window.getWidth() - image.getWidth(null);
-    int windowHeight = window.getHeight() - image.getHeight(null);
-    int x = (int) xLevelPosition * windowWidth / levelWidth;
-    int y = (int) yLevelPosition * windowHeight / levelHeight;
-
-    graphic.drawImage(image, x, y, null);
+  private void drawPacmanRight(Graphics graphic, int x, int y) {
+    int angle = (int) (20 * (Math.sin((x + y) * 2 * Math.PI / PACMAN_RADIUS) + 1));
+    graphic.setColor(Color.YELLOW);
+    graphic.fillArc(x, y, PACMAN_RADIUS, PACMAN_RADIUS, angle / 2, 360 - angle);
   }
 
-  private void drawLeftPacman(Graphics graphic, JFrame window, PacMan pacman, Level level) {
-    Image image = leftPacman.getImage();
-    int xLevelPosition = pacman.getPosition().getX();
-    int yLevelPosition = pacman.getPosition().getY();
-    int levelWidth = level.getWidth();
-    int levelHeight = level.getHeight();
-    int windowWidth = window.getWidth() - image.getWidth(null);
-    int windowHeight = window.getHeight() - image.getHeight(null);
-    int x = (int) xLevelPosition * windowWidth / levelWidth;
-    int y = (int) yLevelPosition * windowHeight / levelHeight;
-
-    graphic.drawImage(image, x, y, null);
+  private void drawPacmanLeft(Graphics graphic, int x, int y) {
+    int direction = 180;
+    int angle = (int) (20 * (Math.sin((x + y) * 2 * Math.PI / PACMAN_RADIUS) + 1));
+    graphic.setColor(Color.YELLOW);
+    graphic.fillArc(x, y, PACMAN_RADIUS, PACMAN_RADIUS, angle / 2 + direction, 360 - angle);
   }
 
-  private void drawUpPacman(Graphics graphic, JFrame window, PacMan pacman, Level level) {
-    Image image = upPacman.getImage();
-    int xLevelPosition = pacman.getPosition().getX();
-    int yLevelPosition = pacman.getPosition().getY();
-    int levelWidth = level.getWidth();
-    int levelHeight = level.getHeight();
-    int windowWidth = window.getWidth() - image.getWidth(null);
-    int windowHeight = window.getHeight() - image.getHeight(null);
-    int x = (int) xLevelPosition * windowWidth / levelWidth;
-    int y = (int) yLevelPosition * windowHeight / levelHeight;
-
-    graphic.drawImage(image, x, y, null);
+  private void drawPacmanUp(Graphics graphic, int x, int y) {
+    int direction = 90;
+    int angle = (int) (20 * (Math.sin((x + y) * 2 * Math.PI / PACMAN_RADIUS) + 1));
+    graphic.setColor(Color.YELLOW);
+    graphic.fillArc(x, y, PACMAN_RADIUS, PACMAN_RADIUS, angle / 2 + direction, 360 - angle);
   }
 
-  private void drawDownPacman(Graphics graphic, JFrame window, PacMan pacman, Level level) {
-    Image image = downPacman.getImage();
-    int xLevelPosition = pacman.getPosition().getX();
-    int yLevelPosition = pacman.getPosition().getY();
-    int levelWidth = level.getWidth();
-    int levelHeight = level.getHeight();
-    int windowWidth = window.getWidth() - image.getWidth(null);
-    int windowHeight = window.getHeight() - image.getHeight(null);
-    int x = (int) xLevelPosition * windowWidth / levelWidth;
-    int y = (int) yLevelPosition * windowHeight / levelHeight;
-
-    graphic.drawImage(image, x, y, null);
+  private void drawPacmanDown(Graphics graphic, int x, int y) {
+    int direction = 270;
+    int angle = (int) (20 * (Math.sin((x + y) * 2 * Math.PI / PACMAN_RADIUS) + 1));
+    graphic.setColor(Color.YELLOW);
+    graphic.fillArc(x, y, PACMAN_RADIUS, PACMAN_RADIUS, angle / 2 + direction, 360 - angle);
   }
 
 }
