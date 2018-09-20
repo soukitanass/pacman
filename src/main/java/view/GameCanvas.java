b57ffb6c6d95e92fa7f2698ba42902bb2b96c9d7
= <webf0x@hotmail.com>
2018-09-19 20:09:53 -0400

    Set the background black

--- src/main/java/view/GameCanvas.java
+++ src/main/java/view/GameCanvas.java
@@ -2,7 +2,6 @@ package view;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.event.KeyListener;
 import javax.swing.JFrame;
@@ -13,18 +12,14 @@ import model.Level;
 
 @SuppressWarnings("serial")
 public class GameCanvas extends JPanel {
-
   private final IGameModel model;
   private PacManPanel pacmanPanel;
   private LevelPanel levelPanel;
 
-  // Constant variables
   private static final int FRAME_WIDTH = 600;
   private static final int FRAME_HEIGHT = 800;
   private static final String GAME_TITLE = "Pac-Man";
 
-
-  private final FlowLayout layoutCenter = new FlowLayout(FlowLayout.CENTER);
   private JLayeredPane layeredPane = new JLayeredPane();
 
   private JFrame window = new JFrame(GAME_TITLE);
@@ -32,29 +27,20 @@ public class GameCanvas extends JPanel {
   GameCanvas(IGameModel model) {
     this.model = model;
 
-    // Setting the frame parameters
     window.setSize(FRAME_WIDTH, FRAME_HEIGHT);
     window.setMinimumSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
     window.setLocationRelativeTo(null);
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.getContentPane().setBackground(Color.BLACK);
 
-    // Add level panel
     levelPanel = new LevelPanel(model);
-    levelPanel.setBounds(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
-    levelPanel.setLayout(layoutCenter);
     levelPanel.setBackground(Color.BLACK);
-    layeredPane.add(levelPanel, JLayeredPane.DEFAULT_LAYER);
+    levelPanel.setOpaque(true);
+    layeredPane.add(levelPanel, Integer.valueOf(1));
 
-
-    // Add Pac-Man panel
     pacmanPanel = new PacManPanel(model);
-    pacmanPanel.setBounds(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
-    pacmanPanel.setLayout(layoutCenter);
     pacmanPanel.setOpaque(false);
-    layeredPane.add(pacmanPanel, Integer.valueOf(1));
+    layeredPane.add(pacmanPanel, Integer.valueOf(2));
 
-    // Add the frame content
     window.add(layeredPane);
     window.add(this);
     window.setVisible(true);
@@ -67,6 +53,8 @@ public class GameCanvas extends JPanel {
 
   @Override
   public void paint(Graphics graphic) {
+    levelPanel.setBounds(0, 0, window.getWidth(), window.getHeight());
+    pacmanPanel.setBounds(0, 0, window.getWidth(), window.getHeight());
     final int pixelTileSize = getPixelTileSize();
     levelPanel.setPixelTileSize(pixelTileSize);
     levelPanel.paint(graphic);
@@ -80,8 +68,8 @@ public class GameCanvas extends JPanel {
 
   public int getPixelTileSize() {
     final Level level = model.getCurrentLevel();
-    final float widthRatio = getWidth() / level.getWidth();
-    final float heightRatio = getHeight() / level.getHeight();
+    final float widthRatio = (float) (getWidth()) / level.getWidth();
+    final float heightRatio = (float) (getHeight()) / level.getHeight();
     return (int) Math.min(Math.floor(widthRatio), Math.floor(heightRatio));
   }
 }
