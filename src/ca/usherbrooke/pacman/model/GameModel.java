8db2cbe75beed248ba5e25102b51a84510dd8cc3
Benjamin Roy <benji015@hotmail.com>
2018-09-24 19:55:16 -0400

    Adding clashpath for travis

--- src/ca/usherbrooke/pacman/model/GameModel.java
+++ src/ca/usherbrooke/pacman/model/GameModel.java
@@ -3,7 +3,6 @@ package ca.usherbrooke.pacman.model;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
