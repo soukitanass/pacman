a9f1dd72b48f98646916e7515659c2a08f660a2f
soukainanassib <30549229+soukainanassib@users.noreply.github.com>
2018-09-25 11:50:58 -0400

    pause panel

--- src/ca/usherbrooke/pacman/model/GameModel.java
+++ src/ca/usherbrooke/pacman/model/GameModel.java
@@ -10,140 +10,140 @@ import ca.usherbrooke.pacman.model.sound.Observer;
 import ca.usherbrooke.pacman.view.utilities.WarningDialog;
 
 public class GameModel implements IGameModel {
-  private Levels levelsList;
+	private Levels levelsList;
 
-  private int currentGameFrame = 0;
-  private boolean isPaused;
-  private boolean isRunning;
-  private MovementManager movementManager;
-  private boolean isGameStarted = false;
-  private PacMan pacman;
-  private PacmanPacgumCollisionManager pacmanPacgumCollisionManager;
-  private PacmanSuperPacgumCollisionManager pacmanSuperPacgumCollisionManager;
-  private List<Observer> observers = new ArrayList<>();
+	private int currentGameFrame = 0;
+	private boolean isPaused;
+	private boolean isRunning;
+	private MovementManager movementManager;
+	private boolean isGameStarted = false;
+	private PacMan pacman;
+	private PacmanPacgumCollisionManager pacmanPacgumCollisionManager;
+	private PacmanSuperPacgumCollisionManager pacmanSuperPacgumCollisionManager;
+	private List<Observer> observers = new ArrayList<>();
 
-  public void attach(Observer observer) {
-    observers.add(observer);
-  }
+	public void attach(Observer observer) {
+		observers.add(observer);
+	}
 
-  @Override
-  public void consumingPacGums() {
-    for (Observer observer : observers) {
-      observer.consumingPacGums();
-    }
-  }
+	@Override
+	public void consumingPacGums() {
+		for (Observer observer : observers) {
+			observer.consumingPacGums();
+		}
+	}
 
-  @Override
-  public void movingToEmptySpace() {
-    for (Observer observer : observers) {
-      observer.movingToEmptySpace();
-    }
-  }
+	@Override
+	public void movingToEmptySpace() {
+		for (Observer observer : observers) {
+			observer.movingToEmptySpace();
+		}
+	}
 
-  @Override
-  public void update() {
-    if (isPaused()) {
-      return;
-    }
-    ++currentGameFrame;
-    if (!isGameStarted()) {
-      startGame();
-    }
-    if (pacmanPacgumCollisionManager.isPacgumConsumed()) {
-      consumingPacGums();
-    } else {
-      movingToEmptySpace();
-    }
-    pacmanSuperPacgumCollisionManager.update();
-    movementManager.updatePacmanPosition();
-  }
+	@Override
+	public void update() {
+		if (isPaused()) {
+			return;
+		}
+		++currentGameFrame;
+		if (!isGameStarted()) {
+			startGame();
+		}
+		if (pacmanPacgumCollisionManager.isPacgumConsumed()) {
+			consumingPacGums();
+		} else {
+			movingToEmptySpace();
+		}
+		pacmanSuperPacgumCollisionManager.update();
+		movementManager.updatePacmanPosition();
+	}
 
-  private void startGame() {
-    Level level = getCurrentLevel();
-    IMoveValidator moveValidator = new MoveValidator(level);
-    movementManager = new MovementManager(pacman, moveValidator);
-    pacmanPacgumCollisionManager = new PacmanPacgumCollisionManager(pacman, level);
-    pacmanSuperPacgumCollisionManager = new PacmanSuperPacgumCollisionManager(pacman, level);
-    isGameStarted = true;
-  }
+	private void startGame() {
+		Level level = getCurrentLevel();
+		IMoveValidator moveValidator = new MoveValidator(level);
+		movementManager = new MovementManager(pacman, moveValidator);
+		pacmanPacgumCollisionManager = new PacmanPacgumCollisionManager(pacman, level);
+		pacmanSuperPacgumCollisionManager = new PacmanSuperPacgumCollisionManager(pacman, level);
+		isGameStarted = true;
+	}
 
-  private boolean isGameStarted() {
-    return isGameStarted;
-  }
+	private boolean isGameStarted() {
+		return isGameStarted;
+	}
 
-  @Override
-  public int getCurrentGameFrame() {
-    return currentGameFrame;
-  }
+	@Override
+	public int getCurrentGameFrame() {
+		return currentGameFrame;
+	}
 
-  @Override
-  public void pause() {
-    isPaused = true;
-  }
+	@Override
+	public void pause() {
+		isPaused = true;
+	}
 
-  @Override
-  public void unpause() {
-    isPaused = false;
-  }
+	@Override
+	public void unpause() {
+		isPaused = false;
+	}
 
-  @Override
-  public boolean isPaused() {
-    return isPaused;
-  }
+	@Override
+	public boolean isPaused() {
+		return isPaused;
+	}
 
-  @Override
-  public void togglePause() {
-    if (isPaused()) {
-      unpause();
-    } else {
-      pause();
-    }
-  }
+	@Override
+	public void togglePause() {
+		if (isPaused()) {
+			unpause();
+		} else {
+			pause();
+		}
+	}
 
-  @Override
-  public void quit() {
-    setRunning(false);
-  }
+	@Override
+	public void quit() {
+		setRunning(false);
+	}
 
-  @Override
-  public boolean isRunning() {
-    return isRunning;
-  }
+	@Override
+	public boolean isRunning() {
+		return isRunning;
+	}
 
-  @Override
-  public void setRunning(boolean isRunning) {
-    this.isRunning = isRunning;
-  }
+	@Override
+	public void setRunning(boolean isRunning) {
+		this.isRunning = isRunning;
+	}
 
-  public Level getCurrentLevel() {
-    final int currentLevel = this.levelsList.getCurrentLevel();
-    final List<Level> levels = this.levelsList.getLevels();
-    return levels.get(currentLevel);
-  }
+	public Level getCurrentLevel() {
+		final int currentLevel = this.levelsList.getCurrentLevel();
+		final List<Level> levels = this.levelsList.getLevels();
+		return levels.get(currentLevel);
+	}
 
-  public void loadLevels(String levelsPath) {
-    Gson gson = new Gson();
+	public void loadLevels(String levelsPath) {
+		Gson gson = new Gson();
 
-    try {
-      File file = new File(GameModel.class.getClassLoader().getResource(levelsPath).getFile());
-      FileReader fileReader = new FileReader(file);    
-      this.levelsList = gson.fromJson(new BufferedReader(fileReader), Levels.class);
-      this.pacman = getCurrentLevel().getPacMan();
-    } catch (Exception exception) {
-      WarningDialog.display("Error while opening level file. ", exception);
-    }
-  }
+		try {
+			File file = new File(GameModel.class.getClassLoader().getResource(levelsPath).getFile());
+			FileReader fileReader = new FileReader(file);
+			this.levelsList = gson.fromJson(new BufferedReader(fileReader), Levels.class);
+			this.pacman = getCurrentLevel().getPacMan();
+		} catch (Exception exception) {
+			WarningDialog.display("Error while opening level file. ", exception);
+		}
+	}
 
-  @Override
-  public PacMan getPacman() {
-    return pacman;
-  }
+	@Override
+	public PacMan getPacman() {
+		return pacman;
+	}
 
-  @Override
-  public void setPacmanDirection(Direction direction) {
-    if (isPaused()) {
-      return;
-    }
-    movementManager.setPacmanDirection(direction);
-  }
+	@Override
+	public void setPacmanDirection(Direction direction) {
+		if (isPaused()) {
+			return;
+		}
+		movementManager.setPacmanDirection(direction);
+	}
 }
--- src/ca/usherbrooke/pacman/view/GameCanvas.java
+++ src/ca/usherbrooke/pacman/view/GameCanvas.java
@@ -11,82 +11,94 @@ import ca.usherbrooke.pacman.model.IGameModel;
 
 @SuppressWarnings("serial")
 public class GameCanvas extends JPanel {
-  private final IGameModel model;
-  private PacManPanel pacmanPanel;
-  private LevelPanel levelPanel;
+	private final IGameModel model;
+	private PacManPanel pacmanPanel;
+	private LevelPanel levelPanel;
+	private PausePanel pausePanel;
 
-  private static final int FRAME_WIDTH = 600;
-  private static final int FRAME_HEIGHT = 800;
-  private static final String GAME_TITLE = "Pac-Man";
+	private static final int FRAME_WIDTH = 600;
+	private static final int FRAME_HEIGHT = 800;
+	private static final String GAME_TITLE = "Pac-Man";
 
-  private JLayeredPane layeredPane = new JLayeredPane();
+	private JLayeredPane layeredPane = new JLayeredPane();
 
-  private JFrame window = new JFrame(GAME_TITLE);
+	private JFrame window = new JFrame(GAME_TITLE);
 
-  GameCanvas(IGameModel model) {
-    this.model = model;
+	GameCanvas(IGameModel model) {
+		this.model = model;
 
-    window.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-    window.setMinimumSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
-    window.setLocationRelativeTo(null);
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		window.setMinimumSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+		window.setLocationRelativeTo(null);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    levelPanel = new LevelPanel(model);
-    levelPanel.setBackground(Color.BLACK);
-    layeredPane.add(levelPanel, Integer.valueOf(1));
+		levelPanel = new LevelPanel(model);
+		levelPanel.setBackground(Color.BLACK);
+		layeredPane.add(levelPanel, Integer.valueOf(1));
 
-    pacmanPanel = new PacManPanel(model);
-    pacmanPanel.setOpaque(false);
-    layeredPane.add(pacmanPanel, Integer.valueOf(2));
+		pacmanPanel = new PacManPanel(model);
+		pacmanPanel.setOpaque(false);
+		layeredPane.add(pacmanPanel, Integer.valueOf(2));
 
-    window.add(layeredPane);
-    window.add(this);
-    window.setVisible(true);
-  }
+		window.add(layeredPane);
+		window.add(this);
+		window.setVisible(true);
+	}
 
-  @Override
-  public void addKeyListener(KeyListener keyListener) {
-    levelPanel.addKeyListener(keyListener);
-  }
+	@Override
+	public void addKeyListener(KeyListener keyListener) {
+		levelPanel.addKeyListener(keyListener);
+	}
 
-  @Override
-  public void paint(Graphics graphic) {
-    levelPanel.setBounds(0, 0, window.getWidth(), window.getHeight());
-    pacmanPanel.setBounds(0, 0, window.getWidth(), window.getHeight());
-    final int pixelTileSize = getPixelTileSize();
-    levelPanel.setOffsetX(getOffsetX());
-    levelPanel.setOffsetY(getOffsetY());
-    levelPanel.setPixelTileSize(pixelTileSize);
-    levelPanel.paint(graphic);
-    pacmanPanel.setOffsetX(getOffsetX());
-    pacmanPanel.setOffsetY(getOffsetY());
-    pacmanPanel.setPixelTileSize(pixelTileSize);
-    pacmanPanel.paint(graphic);
-  }
+	@Override
+	public void paint(Graphics graphic) {
+		levelPanel.setBounds(0, 0, window.getWidth(), window.getHeight());
+		pacmanPanel.setBounds(0, 0, window.getWidth(), window.getHeight());
+		final int pixelTileSize = getPixelTileSize();
+		levelPanel.setOffsetX(getOffsetX());
+		levelPanel.setOffsetY(getOffsetY());
+		levelPanel.setPixelTileSize(pixelTileSize);
+		levelPanel.paint(graphic);
+		pacmanPanel.setOffsetX(getOffsetX());
+		pacmanPanel.setOffsetY(getOffsetY());
+		pacmanPanel.setPixelTileSize(pixelTileSize);
+		pacmanPanel.paint(graphic);
+		if (model.isPaused()) {
+			pausePanel.setBounds(0, 0, window.getWidth(), window.getHeight());
+			pausePanel.paint(graphic);
+		}
 
-  public void dispose() {
-    window.dispose();
-  }
+	}
 
-  public int getPixelTileSize() {
-    final float panelWidthPixels = getWidth();
-    final float panelHeightPixels = getHeight();
-    final int availableWindowWidthPixels =
-        (int) Math.min(panelWidthPixels, 3.0 / 4.0 * panelHeightPixels);
-    final int availableWindowHeightPixels =
-        (int) Math.min(panelHeightPixels, 4.0 / 3.0 * panelWidthPixels);
-    final float widthRatio = (float) (availableWindowWidthPixels / levelPanel.getWidthTiles());
-    final float heightRatio = (float) (availableWindowHeightPixels / levelPanel.getHeightTiles());
-    return (int) Math.min(Math.floor(widthRatio), Math.floor(heightRatio));
-  }
+	public void dispose() {
+		window.dispose();
+	}
 
-  public int getOffsetX() {
-    final int levelWidthPixels = levelPanel.getWidthTiles() * getPixelTileSize();
-    return (getWidth() - levelWidthPixels) / 2;
-  }
+	public int getPixelTileSize() {
+		final float panelWidthPixels = getWidth();
+		final float panelHeightPixels = getHeight();
+		final int availableWindowWidthPixels = (int) Math.min(panelWidthPixels, 3.0 / 4.0 * panelHeightPixels);
+		final int availableWindowHeightPixels = (int) Math.min(panelHeightPixels, 4.0 / 3.0 * panelWidthPixels);
+		final float widthRatio = (float) (availableWindowWidthPixels / levelPanel.getWidthTiles());
+		final float heightRatio = (float) (availableWindowHeightPixels / levelPanel.getHeightTiles());
+		return (int) Math.min(Math.floor(widthRatio), Math.floor(heightRatio));
+	}
 
-  public int getOffsetY() {
-    final int levelHeightPixels = levelPanel.getHeightTiles() * getPixelTileSize();
-    return (getHeight() - levelHeightPixels) / 2;
-  }
+	public int getOffsetX() {
+		final int levelWidthPixels = levelPanel.getWidthTiles() * getPixelTileSize();
+		return (getWidth() - levelWidthPixels) / 2;
+	}
+
+	public int getOffsetY() {
+		final int levelHeightPixels = levelPanel.getHeightTiles() * getPixelTileSize();
+		return (getHeight() - levelHeightPixels) / 2;
+	}
+
+	public void setPausePanel() {
+		pausePanel = new PausePanel(model);
+		pausePanel.setBackground(new Color(0, 0, 0, 80));
+		pausePanel.setOpaque(true);
+		window.add(pausePanel);
+
+	}
 }
