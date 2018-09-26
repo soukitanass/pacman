5b9746e38c8027a8ae1be70ee672fcf6051e7615
Benjamin Roy <benji015@hotmail.com>
2018-09-25 21:25:44 -0400

    The next level starts when the current level is completed (#56)

--- src/ca/usherbrooke/pacman/model/GameModel.java
+++ src/ca/usherbrooke/pacman/model/GameModel.java
@@ -47,7 +47,7 @@ public class GameModel implements IGameModel {
     }
     ++currentGameFrame;
     if (!isGameStarted()) {
-      startGame();
+      startLevel();
     }
     if (pacmanPacgumCollisionManager.isPacgumConsumed()) {
       consumingPacGums();
@@ -56,15 +56,26 @@ public class GameModel implements IGameModel {
     }
     pacmanSuperPacgumCollisionManager.update();
     movementManager.updatePacmanPosition();
+
+    Level level = getCurrentLevel();
+    if (level.isCompleted()) {
+      goToNextLevel();
+    }
   }
 
-  private void startGame() {
+  private void startLevel() {
     Level level = getCurrentLevel();
     IMoveValidator moveValidator = new MoveValidator(level);
+    pacman = level.getPacMan();
     movementManager = new MovementManager(pacman, moveValidator);
     pacmanPacgumCollisionManager = new PacmanPacgumCollisionManager(pacman, level);
     pacmanSuperPacgumCollisionManager = new PacmanSuperPacgumCollisionManager(pacman, level);
     isGameStarted = true;
+  }
+
+  private void goToNextLevel() {
+    this.levelsList.incrementCurrentLevel();
+    startLevel();
   }
 
   private boolean isGameStarted() {
@@ -126,7 +137,7 @@ public class GameModel implements IGameModel {
 
     try {
       File file = new File(GameModel.class.getClassLoader().getResource(levelsPath).getFile());
-      FileReader fileReader = new FileReader(file);    
+      FileReader fileReader = new FileReader(file);
       this.levelsList = gson.fromJson(new BufferedReader(fileReader), Levels.class);
       this.pacman = getCurrentLevel().getPacMan();
     } catch (Exception exception) {
