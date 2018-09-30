122ddb3b54702b9d274b5e27f2ca85a66e7e49d7
= <webf0x@hotmail.com>
2018-09-30 18:26:06 -0400

    Move view in a thread

--- src/ca/usherbrooke/pacman/view/GameView.java
+++ src/ca/usherbrooke/pacman/view/GameView.java
@@ -2,14 +2,21 @@ package ca.usherbrooke.pacman.view;
 
 import java.awt.event.KeyListener;
 import ca.usherbrooke.pacman.model.IGameModel;
+import ca.usherbrooke.pacman.view.utilities.WarningDialog;
 
 public class GameView implements IGameView {
+  private static final String NAME = "RenderThread";
   private GameCanvas canvas;
   private IGameModel model;
+  private boolean shouldRun = false;
+  private long lastUpdateTimeMilliseconds;
+  private int updatePeriodMilliseconds;
 
-
-  public GameView(IGameModel model, int ghostSpriteTogglePeriod, int pacmanSpriteTogglePeriod) {
+  public GameView(IGameModel model, int ghostSpriteTogglePeriod, int pacmanSpriteTogglePeriod,
+      int updatePeriodMilliseconds) {
+    lastUpdateTimeMilliseconds = System.currentTimeMillis();
     this.model = model;
+    this.updatePeriodMilliseconds = updatePeriodMilliseconds;
     canvas = new GameCanvas(model, ghostSpriteTogglePeriod, pacmanSpriteTogglePeriod);
   }
 
@@ -32,6 +39,7 @@ public class GameView implements IGameView {
   @Override
   public void close() {
     canvas.dispose();
+    shouldRun = false;
   }
 
   public GameCanvas getCanvas() {
@@ -40,6 +48,44 @@ public class GameView implements IGameView {
 
   public void setCanvas(GameCanvas canvas) {
     this.canvas = canvas;
+  }
+
+  @Override
+  public void run() {
+    System.out.println("START - " + this.getName());
+    shouldRun = true;
+    while (shouldRun) {
+      final long currentTimeMillis = System.currentTimeMillis();
+      if (isTimeToUpdate(currentTimeMillis)) {
+        lastUpdateTimeMilliseconds = currentTimeMillis;
+        update();
+      }
+      sleepUntilNextUpdate();
+    }
+    System.out.println("STOP - " + this.getName());
+  }
+
+  private boolean isTimeToUpdate(long currentTimeMilliseconds) {
+    return currentTimeMilliseconds >= lastUpdateTimeMilliseconds + updatePeriodMilliseconds;
+  }
+
+  private void sleepUntilNextUpdate() {
+    final long timeSinceLastUpdateMilliseconds =
+        System.currentTimeMillis() - lastUpdateTimeMilliseconds;
+    final long timeUntilNextUpdateMilliseconds =
+        updatePeriodMilliseconds - timeSinceLastUpdateMilliseconds;
+    if (timeUntilNextUpdateMilliseconds <= 0) {
+      return;
+    }
+    try {
+      Thread.sleep(timeUntilNextUpdateMilliseconds);
+    } catch (InterruptedException e) {
+      WarningDialog.display("An error occured while waiting for the next frame update", e);
+    }
+  }
+
+  private String getName() {
+    return NAME;
   }
 
 }
