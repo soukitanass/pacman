555fd672587f834eb7e4e95804fa302215c96a8e
= <webf0x@hotmail.com>
2018-10-02 23:19:45 -0400

    Test RenderThread

---
+++ test/ca/usherbrooke/pacman/game/FakeTimeGetter.java
@@ -0,0 +1,20 @@
+package ca.usherbrooke.pacman.game;
+
+public class FakeTimeGetter implements ITimeGetter {
+
+  private long currentTimeMilliseconds;
+
+  public FakeTimeGetter(int currentTimeMilliseconds) {
+    setCurrentTime(currentTimeMilliseconds);
+  }
+
+  @Override
+  public long getMillis() {
+    return currentTimeMilliseconds;
+  }
+
+  public void setCurrentTime(long currentTimeMilliseconds) {
+    this.currentTimeMilliseconds = currentTimeMilliseconds;
+  }
+
+}
