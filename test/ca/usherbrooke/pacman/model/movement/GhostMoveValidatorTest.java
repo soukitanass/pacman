2f9070bf74d448989a4f5dc763b79dacd98c7204
= <webf0x@hotmail.com>
2018-10-24 17:01:47 -0400

    Refactor movement tests, extract duplicated code

--- test/ca/usherbrooke/pacman/model/movement/GhostMoveValidatorTest.java
+++ test/ca/usherbrooke/pacman/model/movement/GhostMoveValidatorTest.java
@@ -9,10 +9,6 @@
 package ca.usherbrooke.pacman.model.movement;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import org.junit.Test;
 import ca.usherbrooke.pacman.model.direction.Direction;
 import ca.usherbrooke.pacman.model.exceptions.InvalidDirectionException;
@@ -21,139 +17,70 @@ import ca.usherbrooke.pacman.model.movements.IMoveRequest;
 import ca.usherbrooke.pacman.model.movements.IMoveValidator;
 import ca.usherbrooke.pacman.model.movements.MoveRequest;
 import ca.usherbrooke.pacman.model.objects.Level;
+import ca.usherbrooke.pacman.model.objects.MockLevelFactory;
 import ca.usherbrooke.pacman.model.position.Position;
 
 public class GhostMoveValidatorTest {
 
   @Test
   public void movesToEmptySpacesAreValid() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelTwoByTwoEmpty();
+    Level mockLevel = MockLevelFactory.getMockLevelThreeByThreeEmpty();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
-    IMoveRequest moveLeft = new MoveRequest(new Position(1, 0), Direction.LEFT);
-    IMoveRequest moveRight = new MoveRequest(new Position(0, 0), Direction.RIGHT);
-    IMoveRequest moveUp = new MoveRequest(new Position(0, 1), Direction.UP);
-    IMoveRequest moveDown = new MoveRequest(new Position(0, 0), Direction.DOWN);
-    assertTrue(moveValidator.isValid(moveLeft));
-    assertTrue(moveValidator.isValid(moveRight));
-    assertTrue(moveValidator.isValid(moveUp));
-    assertTrue(moveValidator.isValid(moveDown));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveLeft));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveRight));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveUp));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveDown));
+    MoveValidatorTestUtility.assertCanMoveInAnyDirection(moveValidator, new Position(1, 1));
+    MoveValidatorTestUtility.assertCanFaceAnyDirection(moveValidator, new Position(1, 1));
   }
 
   @Test
   public void movesIntoAWallAreInvalid() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelTwoByTwoWalls();
+    Level mockLevel = MockLevelFactory.getMockLevelThreeByThreeWalls();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
-    IMoveRequest moveLeft = new MoveRequest(new Position(1, 0), Direction.LEFT);
-    IMoveRequest moveRight = new MoveRequest(new Position(0, 0), Direction.RIGHT);
-    IMoveRequest moveUp = new MoveRequest(new Position(0, 1), Direction.UP);
-    IMoveRequest moveDown = new MoveRequest(new Position(0, 0), Direction.DOWN);
-    assertFalse(moveValidator.isValid(moveLeft));
-    assertFalse(moveValidator.isValid(moveRight));
-    assertFalse(moveValidator.isValid(moveUp));
-    assertFalse(moveValidator.isValid(moveDown));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveLeft));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveRight));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveUp));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveDown));
+    MoveValidatorTestUtility.assertCannotMoveInAnyDirection(moveValidator, new Position(1, 1));
+    MoveValidatorTestUtility.assertCannotFaceAnyDirection(moveValidator, new Position(1, 1));
   }
 
   @Test
   public void movesIntoATunnelAreInvalid() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelTwoByTwoTunnels();
+    Level mockLevel = MockLevelFactory.getMockLevelThreeByThreeTunnels();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
-    IMoveRequest moveLeft = new MoveRequest(new Position(1, 0), Direction.LEFT);
-    IMoveRequest moveRight = new MoveRequest(new Position(0, 0), Direction.RIGHT);
-    IMoveRequest moveUp = new MoveRequest(new Position(0, 1), Direction.UP);
-    IMoveRequest moveDown = new MoveRequest(new Position(0, 0), Direction.DOWN);
-    assertFalse(moveValidator.isValid(moveLeft));
-    assertFalse(moveValidator.isValid(moveRight));
-    assertFalse(moveValidator.isValid(moveUp));
-    assertFalse(moveValidator.isValid(moveDown));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveLeft));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveRight));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveUp));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveDown));
+    MoveValidatorTestUtility.assertCannotMoveInAnyDirection(moveValidator, new Position(1, 1));
+    MoveValidatorTestUtility.assertCannotFaceAnyDirection(moveValidator, new Position(1, 1));
   }
 
   @Test
   public void movesFromGhostRoomToGhostGatesAreValid() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelSingleGhostRoomSurroundedByGhostGates();
+    Level mockLevel = MockLevelFactory.getMockLevelSingleGhostRoomSurroundedByGhostGates();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
-    IMoveRequest moveLeft = new MoveRequest(new Position(1, 1), Direction.LEFT);
-    IMoveRequest moveRight = new MoveRequest(new Position(1, 1), Direction.RIGHT);
-    IMoveRequest moveUp = new MoveRequest(new Position(1, 1), Direction.UP);
-    IMoveRequest moveDown = new MoveRequest(new Position(1, 1), Direction.DOWN);
-    assertTrue(moveValidator.isValid(moveLeft));
-    assertTrue(moveValidator.isValid(moveRight));
-    assertTrue(moveValidator.isValid(moveUp));
-    assertTrue(moveValidator.isValid(moveDown));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveLeft));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveRight));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveUp));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveDown));
+    MoveValidatorTestUtility.assertCanMoveInAnyDirection(moveValidator, new Position(1, 1));
+    MoveValidatorTestUtility.assertCanFaceAnyDirection(moveValidator, new Position(1, 1));
   }
 
   @Test
   public void movesFromGhostGateToGhostRoomsAreInvalid() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelSingleGhostGateSurroundedByGhostRooms();
+    Level mockLevel = MockLevelFactory.getMockLevelSingleGhostGateSurroundedByGhostRooms();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
-    IMoveRequest moveLeft = new MoveRequest(new Position(1, 1), Direction.LEFT);
-    IMoveRequest moveRight = new MoveRequest(new Position(1, 1), Direction.RIGHT);
-    IMoveRequest moveUp = new MoveRequest(new Position(1, 1), Direction.UP);
-    IMoveRequest moveDown = new MoveRequest(new Position(1, 1), Direction.DOWN);
-    assertFalse(moveValidator.isValid(moveLeft));
-    assertFalse(moveValidator.isValid(moveRight));
-    assertFalse(moveValidator.isValid(moveUp));
-    assertFalse(moveValidator.isValid(moveDown));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveLeft));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveRight));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveUp));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveDown));
+    MoveValidatorTestUtility.assertCannotMoveInAnyDirection(moveValidator, new Position(1, 1));
+    MoveValidatorTestUtility.assertCannotFaceAnyDirection(moveValidator, new Position(1, 1));
   }
 
   @Test
   public void movesFromGhostGateToEmptyAreValid() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelSingleGhostGateSurroundedByEmptiness();
+    Level mockLevel = MockLevelFactory.getMockLevelSingleGhostGateSurroundedByEmptiness();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
-    IMoveRequest moveLeft = new MoveRequest(new Position(1, 1), Direction.LEFT);
-    IMoveRequest moveRight = new MoveRequest(new Position(1, 1), Direction.RIGHT);
-    IMoveRequest moveUp = new MoveRequest(new Position(1, 1), Direction.UP);
-    IMoveRequest moveDown = new MoveRequest(new Position(1, 1), Direction.DOWN);
-    assertTrue(moveValidator.isValid(moveLeft));
-    assertTrue(moveValidator.isValid(moveRight));
-    assertTrue(moveValidator.isValid(moveUp));
-    assertTrue(moveValidator.isValid(moveDown));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveLeft));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveRight));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveUp));
-    assertTrue(moveValidator.isDesiredDirectionValid(moveDown));
+    MoveValidatorTestUtility.assertCanMoveInAnyDirection(moveValidator, new Position(1, 1));
+    MoveValidatorTestUtility.assertCanFaceAnyDirection(moveValidator, new Position(1, 1));
   }
 
   @Test
   public void movesFromEmptyToGhostGateAreInvalid() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelSingleEmptySurroundedByGhostGates();
+    Level mockLevel = MockLevelFactory.getMockLevelSingleEmptySurroundedByGhostGates();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
-    IMoveRequest moveLeft = new MoveRequest(new Position(1, 1), Direction.LEFT);
-    IMoveRequest moveRight = new MoveRequest(new Position(1, 1), Direction.RIGHT);
-    IMoveRequest moveUp = new MoveRequest(new Position(1, 1), Direction.UP);
-    IMoveRequest moveDown = new MoveRequest(new Position(1, 1), Direction.DOWN);
-    assertFalse(moveValidator.isValid(moveLeft));
-    assertFalse(moveValidator.isValid(moveRight));
-    assertFalse(moveValidator.isValid(moveUp));
-    assertFalse(moveValidator.isValid(moveDown));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveLeft));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveRight));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveUp));
-    assertFalse(moveValidator.isDesiredDirectionValid(moveDown));
+    MoveValidatorTestUtility.assertCannotMoveInAnyDirection(moveValidator, new Position(1, 1));
+    MoveValidatorTestUtility.assertCannotFaceAnyDirection(moveValidator, new Position(1, 1));
   }
 
   @Test
   public void getTargetPositionNoWrapAround() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelTwoByTwoEmpty();
+    Level mockLevel = MockLevelFactory.getMockLevelTwoByTwoEmpty();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
     IMoveRequest moveLeft = new MoveRequest(new Position(1, 0), Direction.LEFT);
     IMoveRequest moveRight = new MoveRequest(new Position(0, 0), Direction.RIGHT);
@@ -167,7 +94,7 @@ public class GhostMoveValidatorTest {
 
   @Test
   public void getTargetPositionWithWrapAround() throws InvalidDirectionException {
-    Level mockLevel = getMockLevelTwoByTwoEmpty();
+    Level mockLevel = MockLevelFactory.getMockLevelTwoByTwoEmpty();
     IMoveValidator moveValidator = new GhostMoveValidator(mockLevel);
     IMoveRequest moveLeft = new MoveRequest(new Position(0, 0), Direction.LEFT);
     IMoveRequest moveRight = new MoveRequest(new Position(1, 0), Direction.RIGHT);
@@ -177,141 +104,6 @@ public class GhostMoveValidatorTest {
     assertEquals(new Position(0, 0), moveValidator.getTargetPosition(moveRight));
     assertEquals(new Position(0, 1), moveValidator.getTargetPosition(moveUp));
     assertEquals(new Position(0, 0), moveValidator.getTargetPosition(moveDown));
-  }
-
-  // E = Empty
-  //
-  // E | E
-  // ------
-  // E | E
-  private Level getMockLevelTwoByTwoEmpty() {
-    Level mockLevel = mock(Level.class);
-    when(mockLevel.getWidth()).thenReturn(2);
-    when(mockLevel.getHeight()).thenReturn(2);
-    when(mockLevel.isWall(new Position(0, 0))).thenReturn(false);
-    when(mockLevel.isWall(new Position(1, 0))).thenReturn(false);
-    when(mockLevel.isWall(new Position(0, 1))).thenReturn(false);
-    when(mockLevel.isWall(new Position(1, 1))).thenReturn(false);
-    return mockLevel;
-  }
-
-  // W = Wall
-  //
-  // W | W
-  // ------
-  // W | W
-  private Level getMockLevelTwoByTwoWalls() {
-    Level mockLevel = mock(Level.class);
-    when(mockLevel.getWidth()).thenReturn(2);
-    when(mockLevel.getHeight()).thenReturn(2);
-    when(mockLevel.isWall(new Position(0, 0))).thenReturn(true);
-    when(mockLevel.isWall(new Position(1, 0))).thenReturn(true);
-    when(mockLevel.isWall(new Position(0, 1))).thenReturn(true);
-    when(mockLevel.isWall(new Position(1, 1))).thenReturn(true);
-    return mockLevel;
-  }
-
-  // T = Tunnel
-  //
-  // T | T
-  // -----
-  // T | T
-  private Level getMockLevelTwoByTwoTunnels() {
-    Level mockLevel = mock(Level.class);
-    when(mockLevel.getWidth()).thenReturn(2);
-    when(mockLevel.getHeight()).thenReturn(2);
-    when(mockLevel.isTunnel(new Position(0, 0))).thenReturn(true);
-    when(mockLevel.isTunnel(new Position(1, 0))).thenReturn(true);
-    when(mockLevel.isTunnel(new Position(0, 1))).thenReturn(true);
-    when(mockLevel.isTunnel(new Position(1, 1))).thenReturn(true);
-    return mockLevel;
-  }
-
-  // G = Ghost gate
-  // R = Ghost room
-  //
-  // G | G | G
-  // ---------
-  // G | R | G
-  // ---------
-  // G | G | G
-  private Level getMockLevelSingleGhostRoomSurroundedByGhostGates() {
-    Level mockLevel = mock(Level.class);
-    when(mockLevel.getWidth()).thenReturn(3);
-    when(mockLevel.getHeight()).thenReturn(3);
-    when(mockLevel.isGhostGate(new Position(0, 0))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(1, 0))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(2, 0))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(0, 1))).thenReturn(true);
-    when(mockLevel.isGhostRoom(new Position(1, 1))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(2, 1))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(0, 2))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(1, 2))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(2, 2))).thenReturn(true);
-    return mockLevel;
-  }
-
-  // G = Ghost gate
-  // R = Ghost room
-  //
-  // R | R | R
-  // ---------
-  // R | G | R
-  // ---------
-  // R | R | R
-  private Level getMockLevelSingleGhostGateSurroundedByGhostRooms() {
-    Level mockLevel = mock(Level.class);
-    when(mockLevel.getWidth()).thenReturn(3);
-    when(mockLevel.getHeight()).thenReturn(3);
-    when(mockLevel.isGhostRoom(new Position(0, 0))).thenReturn(true);
-    when(mockLevel.isGhostRoom(new Position(1, 0))).thenReturn(true);
-    when(mockLevel.isGhostRoom(new Position(2, 0))).thenReturn(true);
-    when(mockLevel.isGhostRoom(new Position(0, 1))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(1, 1))).thenReturn(true);
-    when(mockLevel.isGhostRoom(new Position(2, 1))).thenReturn(true);
-    when(mockLevel.isGhostRoom(new Position(0, 2))).thenReturn(true);
-    when(mockLevel.isGhostRoom(new Position(1, 2))).thenReturn(true);
-    when(mockLevel.isGhostRoom(new Position(2, 2))).thenReturn(true);
-    return mockLevel;
-  }
-
-  // G = Ghost gate
-  // E = Empty
-  //
-  // E | E | E
-  // ---------
-  // E | G | E
-  // ---------
-  // E | E | E
-  private Level getMockLevelSingleGhostGateSurroundedByEmptiness() {
-    Level mockLevel = mock(Level.class);
-    when(mockLevel.getWidth()).thenReturn(3);
-    when(mockLevel.getHeight()).thenReturn(3);
-    when(mockLevel.isGhostGate(new Position(1, 1))).thenReturn(true);
-    return mockLevel;
-  }
-
-  // G = Ghost gate
-  // E = Empty
-  //
-  // G | G | G
-  // ---------
-  // G | E | G
-  // ---------
-  // G | G | G
-  private Level getMockLevelSingleEmptySurroundedByGhostGates() {
-    Level mockLevel = mock(Level.class);
-    when(mockLevel.getWidth()).thenReturn(3);
-    when(mockLevel.getHeight()).thenReturn(3);
-    when(mockLevel.isGhostGate(new Position(0, 0))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(1, 0))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(2, 0))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(0, 1))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(2, 1))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(0, 2))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(1, 2))).thenReturn(true);
-    when(mockLevel.isGhostGate(new Position(2, 2))).thenReturn(true);
-    return mockLevel;
   }
 
 }
