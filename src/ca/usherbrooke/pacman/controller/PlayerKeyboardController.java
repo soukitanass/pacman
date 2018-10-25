5935315ad9e95611bb8418fa637af73471d4fc5b
Benjamin Roy <benji015@hotmail.com>
2018-10-25 12:31:41 -0400

    Revert "KeyBoardController refactoring (#124)"
    This reverts commit bfe54f34dd6d30d632e559ba552fa7dad9f2b2e0.

--- src/ca/usherbrooke/pacman/controller/PlayerKeyboardController.java
+++ src/ca/usherbrooke/pacman/controller/PlayerKeyboardController.java
@@ -8,45 +8,73 @@
  ******************************************************************************/
 package ca.usherbrooke.pacman.controller;
 
+import static java.awt.event.KeyEvent.VK_P;
+import static java.awt.event.KeyEvent.VK_Q;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.util.List;
+import ca.usherbrooke.pacman.model.GameState;
 import ca.usherbrooke.pacman.model.IGameModel;
+import ca.usherbrooke.pacman.model.direction.Direction;
 import ca.usherbrooke.pacman.view.IGameView;
 
 public class PlayerKeyboardController implements IGameController, KeyListener {
-  private InputHandler inputHandler;
-  private List<KeyEvent> commands;
+  private final IGameModel model;
 
   public PlayerKeyboardController(IGameModel model, IGameView view) {
-    inputHandler = new InputHandler(model);
+    this.model = model;
     view.getCanvas().setPausePanel();
-    commands = new ArrayList<KeyEvent>();
   }
 
   @Override
   public void update() {
-    for (KeyEvent keyEvent : commands) {
-      inputHandler.execute(keyEvent);
-    }
-    commands.clear();
-  }
-
-  @Override
-  public void keyPressed(KeyEvent keyEvent) {
-    commands.add(keyEvent);
-  }
-
-  @Override
-  public void keyReleased(KeyEvent e) {
-    // do not remove
-
+    // Do not remove!
   }
 
   @Override
   public void keyTyped(KeyEvent e) {
-    // do not remove
+    // Do not remove!
+  }
+
+  @Override
+  public void keyPressed(KeyEvent keyEvent) {
+    switch (keyEvent.getKeyCode()) {
+      case VK_P:
+        model.togglePause(true);
+        break;
+      case VK_Q:
+        model.quit();
+        break;
+      case KeyEvent.VK_RIGHT:
+        model.setDirection(model.getPacman(), Direction.RIGHT);
+        break;
+      case KeyEvent.VK_LEFT:
+        model.setDirection(model.getPacman(), Direction.LEFT);
+        break;
+      case KeyEvent.VK_UP:
+        model.setDirection(model.getPacman(), Direction.UP);
+        break;
+      case KeyEvent.VK_DOWN:
+        model.setDirection(model.getPacman(), Direction.DOWN);
+        break;
+      case KeyEvent.VK_ESCAPE:
+        pauseGameInProgress();
+        model.setGameState(GameState.GAME_MENU);
+        break;
+      default:
+        break;
+    }
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    // Do not remove!
+  }
+
+  private void pauseGameInProgress() {
+    if (!model.isGameOver()) {
+      model.pause();
+      model.setManuallyPaused(true);
+    }
   }
 
 }
