soukitanass/pacman

commit b510e3a9aa3871429d778488e834192651eb5b9d

Fix bug where pacgums would respawn after pacman's death

src/ca/usherbrooke/pacman/model/GameModel.java
@@ -274,7 +274,9 @@ public class GameModel implements IGameModel {
 
   @Override
   public void initializeLevel() {
-    this.level = new Level(getInitialLevel());
+    List<List<Integer>> levelMapBeforeInitializing = getCurrentLevel().getMap();
+    setCurrentLevel(new Level(getInitialLevel()));
+    getCurrentLevel().setMap(levelMapBeforeInitializing);
     pacman = level.getPacMan();
     initializeGhosts();
     initializeCollisionManagers();

test/ca/usherbrooke/pacman/model/GameModelTest.java
@@ -158,4 +158,14 @@ public class GameModelTest {
     model.processGhostKilled(model.getCurrentLevel().getGhosts().get(0));
     assertEquals(Integer.valueOf(3000), model.getScore());
   }
+
+  @Test
+  public void whenInitializingLevelThenEatenPacgumsRemainEaten() {
+    final Position positionWithInitialPacgum = new Position(1, 1);
+    assertTrue(model.getCurrentLevel().isPacgum(positionWithInitialPacgum));
+    model.getCurrentLevel().setEmptyMapTile(positionWithInitialPacgum);
+    assertFalse(model.getCurrentLevel().isPacgum(positionWithInitialPacgum));
+    model.initializeLevel();
+    assertFalse(model.getCurrentLevel().isPacgum(positionWithInitialPacgum));
+  }
 }

commit f891652e4d88f3fa774ea1ce7dfc8b5369733f58

Load next level with pacgums when finishing level
Fix bug was introduced in: b510e3a9aa3871429d778488e834192651eb5b9d

src/ca/usherbrooke/pacman/model/GameModel.java
@@ -184,6 +184,7 @@ public class GameModel implements IGameModel {
       onInterruption();
       return;
     }
+    ++currentGameFrame;
     if (isPacmanDead) {
       return;
     }
@@ -194,7 +195,6 @@ public class GameModel implements IGameModel {
       updateIsLevelCompleted();
       return;
     }
-    ++currentGameFrame;
 
     processAllPhysicsEvents();
 
@@ -318,9 +318,12 @@ public class GameModel implements IGameModel {
 
   @Override
   public void initializeLevel() {
+    final boolean isLevelCompleted = getCurrentLevel().isCompleted();
     List<List<Integer>> levelMapBeforeInitializing = getCurrentLevel().getMap();
     setCurrentLevel(new Level(getInitialLevel()));
-    getCurrentLevel().setMap(levelMapBeforeInitializing);
+    if (!isLevelCompleted) {
+      getCurrentLevel().setMap(levelMapBeforeInitializing);
+    }
     pacman = level.getPacMan();
     initializeGhostsDirectionManagers();
     initializeCollisionManagers();
@@ -420,7 +423,7 @@ public class GameModel implements IGameModel {
 
   @Override
   public PacMan getPacman() {
-    return pacman;
+    return level.getPacMan();
   }
 
   @Override

test/ca/usherbrooke/pacman/model/GameModelTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import ca.usherbrooke.pacman.game.Game;
+import ca.usherbrooke.pacman.model.direction.Direction;
 import ca.usherbrooke.pacman.model.objects.Ghost;
 import ca.usherbrooke.pacman.model.objects.Level;
 import ca.usherbrooke.pacman.model.position.Position;
@@ -169,4 +170,36 @@ public class GameModelTest {
     model.initializeLevel();
     assertFalse(model.getCurrentLevel().isPacgum(positionWithInitialPacgum));
   }
+
+  @Test
+  public void whenLevelIsCompletedThenLoadNextLevel() throws InterruptedException {
+    GameModel model = new GameModel(Game.loadLevel("TwoByOneLevelWithPacmanAndPacgum.json"));
+    model.startNewGame();
+    model.setGameState(GameState.GAME);
+    assertTrue(model.getCurrentLevel().isPacgum(new Position(1, 0)));
+    assertEquals(0, model.getCurrentLevelIndex());
+
+    model.setDirection(model.getPacman(), Direction.RIGHT);
+    while (!model.isLevelCompleted()) {
+      model.update();
+      Thread.sleep(100);
+    }
+    while (1 != model.getCurrentLevelIndex()) {
+      model.update();
+    }
+    assertFalse(model.isLevelCompleted());
+    assertTrue(model.getCurrentLevel().isPacgum(new Position(1, 0)));
+
+    model.setDirection(model.getPacman(), Direction.RIGHT);
+    while (!model.isLevelCompleted()) {
+      model.update();
+      Thread.sleep(100);
+    }
+    while (2 != model.getCurrentLevelIndex()) {
+      model.update();
+    }
+    assertFalse(model.isLevelCompleted());
+    assertTrue(model.getCurrentLevel().isPacgum(new Position(1, 0)));
+  }
+
 }