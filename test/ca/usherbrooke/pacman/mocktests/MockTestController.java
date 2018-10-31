78069f4fb345ef8f6d047101fccf02635da6ef98
= <webf0x@hotmail.com>
2018-10-31 13:28:47 -0400

    Make mocktests more verbose
    - Print tapped keys, clicked components' name and spinner's new value

--- test/ca/usherbrooke/pacman/mocktests/MockTestController.java
+++ test/ca/usherbrooke/pacman/mocktests/MockTestController.java
@@ -4,6 +4,7 @@ import java.awt.AWTException;
 import java.awt.Point;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -24,6 +25,7 @@ public class MockTestController {
   }
 
   public void tapKey(int key) throws InterruptedException {
+    log("Tap key: " + KeyEvent.getKeyText(key));
     robot.keyPress(key);
     robot.keyRelease(key);
     sleep();
@@ -31,7 +33,7 @@ public class MockTestController {
 
   public void clickStartOrResumeGame() throws InterruptedException {
     JLabel label = view.getCanvas().getMenuPanel().getStartGameLabel();
-    clickComponent(label);
+    clickComponent(label, "Start/resume game");
   }
 
   private void clickPosition(final int x, final int y) throws InterruptedException {
@@ -51,20 +53,22 @@ public class MockTestController {
 
   public void clickAudio() throws InterruptedException {
     JLabel label = view.getCanvas().getMenuPanel().getAudioLabel();
-    clickComponent(label);
+    clickComponent(label, "Audio");
   }
 
   public void clickAudioGoBack() throws InterruptedException {
     JLabel label = view.getCanvas().getAudioPanel().getGoBackLabel();
-    clickComponent(label);
+    clickComponent(label, "Go back");
   }
 
   public void clickMuteMusicCheckbox() throws InterruptedException {
     JCheckBox checkbox = view.getCanvas().getAudioPanel().getMuteMusicCheckbox();
-    clickComponent(checkbox);
+    clickComponent(checkbox, "Mute music checkbox");
   }
 
-  private void clickComponent(JComponent component) throws InterruptedException {
+  private void clickComponent(JComponent component, String componentDescription)
+      throws InterruptedException {
+    log("Click: " + componentDescription);
     Point locationOnScreen = component.getLocationOnScreen();
     final int labelCenterX = locationOnScreen.x + component.getWidth() / 2;
     final int labelCenterY = locationOnScreen.y + component.getHeight() / 2;
@@ -73,37 +77,43 @@ public class MockTestController {
 
   public void clickMuteSoundCheckbox() throws InterruptedException {
     JCheckBox checkbox = view.getCanvas().getAudioPanel().getMuteSoundCheckbox();
-    clickComponent(checkbox);
+    clickComponent(checkbox, "Mute sound checkbox");
   }
 
   public void clickHighscores() throws InterruptedException {
     JLabel label = view.getCanvas().getMenuPanel().getHighscoresLabel();
-    clickComponent(label);
+    clickComponent(label, "Highscores");
   }
 
   public void clickHighscoreGoBack() throws InterruptedException {
     JLabel label = view.getCanvas().getHighscoresPanel().getGoBackLabel();
-    clickComponent(label);
+    clickComponent(label, "Go back");
   }
 
   public void clickFramesPerSecondCheckbox() throws InterruptedException {
     JCheckBox checkbox = view.getCanvas().getMenuPanel().getFramesPerSecondCheckbox();
-    clickComponent(checkbox);
+    clickComponent(checkbox, "FPS checkbox");
   }
 
   public void setFramesPerSecond(int framesPerSecond) throws InterruptedException {
     JSpinner fpsSpinner = view.getCanvas().getMenuPanel().getFpsSpinner();
-    setSpinnerValue(fpsSpinner, framesPerSecond);
+    setSpinnerValue(fpsSpinner, framesPerSecond, "FPS spinner");
   }
 
-  private void setSpinnerValue(JSpinner spinner, int newValue) throws InterruptedException {
+  private void setSpinnerValue(JSpinner spinner, int newValue, String spinnerDescription)
+      throws InterruptedException {
+    log("Set value to " + newValue + ": " + spinnerDescription);
     spinner.setValue(newValue);
     sleep();
   }
 
   public void clickExitGame() throws InterruptedException {
     JLabel label = view.getCanvas().getMenuPanel().getExitGameLabel();
-    clickComponent(label);
+    clickComponent(label, "Exit game");
+  }
+
+  private void log(String logMessage) {
+    System.out.println(logMessage);
   }
 
 }
